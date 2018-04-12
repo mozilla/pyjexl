99385c895975c40c5637edc109b0abe641fe21ab
Peter Bengtsson <peterbe@mozilla.com>
2018-04-12 08:11:05 -0400

    future is not installed, fixes #15 (#16)

--- setup.py
+++ setup.py
@@ -64,7 +64,7 @@ setup(
     # your project is installed. For an analysis of "install_requires" vs pip's
     # requirements files see:
     # https://packaging.python.org/en/latest/requirements.html
-    install_requires=['parsimonious'],
+    install_requires=['parsimonious', 'future'],
 
     # List additional groups of dependencies here (e.g. development
     # dependencies). You can install these using the following syntax,
